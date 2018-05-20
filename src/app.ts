4113558d67b4a2766ffd9229b6e49eb5d7b4768e
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-20 12:14:48 -0500

    Removing util folder and config folder non necesary

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,10 @@ import express from "express";
 
 
 // Controllers (route handlers)
-import * as homeController from "./controllers/home";
-import * as userController from "./controllers/user";
-import * as apiController from "./controllers/api";
-import * as contactController from "./controllers/contact";
+
 import * as projectManagerMiddleware from "./middleware/project-managers";
 // API keys and Passport configuration
-import * as passportConfig from "./config/passport";
+
 
 // Create Express server
 const app = express();
