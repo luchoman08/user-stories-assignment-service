df9b7e269fb198359778d23a77fd6eb97410cad0
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-08-06 11:09:21 -0500

    Add punctuations attribute to user story and deeloper

--- src/models/Developer.ts
+++ src/models/Developer.ts
@@ -1,8 +1,11 @@
+import { Punctuation } from "./Punctuation";
+
 export class Developer {
     id: string;
     full_name: string;
     role_name: string;
     available_hours_per_week: number;
     color: string;
+    punctuations: Punctuation[];
     constructor() {}
 }
--- src/models/UserStory.ts
+++ src/models/UserStory.ts
@@ -1,7 +1,10 @@
+import { Punctuation } from "./Punctuation";
+
 export class UserStory {
     id: string;
     total_points: number;
     subject: string;
     assigned_to: string; // Developer than is assigned to
+    punctuations: Punctuation[];
     constructor() {}
 }
