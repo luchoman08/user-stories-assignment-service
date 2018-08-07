853cfce6356777906fef557ac3680a43446ffc6b
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-08-07 14:26:38 -0500

    Assignment by punctutations added

---
+++ src/models/AssignmentByPunctuations.ts
@@ -0,0 +1,8 @@
+import { Developer } from "./Developer";
+import { UserStory } from "./UserStory";
+
+export class AssignmentByPunctuation  {
+    developers: Developer[];
+    userStories: UserStory[];
+    assign_same_quantity_of_tasks: boolean;
+}
