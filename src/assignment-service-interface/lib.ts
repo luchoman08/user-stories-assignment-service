cba601a20f0f458fb1dc9099737eff308ee505e5
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-08-07 14:24:01 -0500

    Aditional functions for assignment with attributes added

--- src/assignment-service-interface/lib.ts
+++ src/assignment-service-interface/lib.ts
@@ -1,36 +1,49 @@
 import {
     PulpAssignmentUniqueCost,
+    PulpAssignmentWithAttributes,
+    PulpAssignmentUniqueCostResponse,
+    PulpAssignmentWithAttributesResponse,
     Task,
+    TaskWithAtributes,
     Agent,
-    PulpAssignmentUniqueCostResponse
- } from "./models";
+    AgentWithAttributes
+} from "./models";
 
 import {
     AssignmentUniqueCost,
+    AssignmentByPunctuation,
     Developer,
     UserStory
-     } from "../models";
+} from "../models";
+import { Punctuation } from "../models/Punctuation";
+import { AtributePunctuation } from "./models/AtributePunctuation";
+
 
 export function assignmentUniqueCostToPulpAssignmentUniqueCost(
     assignmentUniqueCost: AssignmentUniqueCost): PulpAssignmentUniqueCost {
-        const pulpAssignmentUniqueCost: PulpAssignmentUniqueCost = new PulpAssignmentUniqueCost();
-        pulpAssignmentUniqueCost.agents =
-            developersToAgents(
-                assignmentUniqueCost.developers,
-                assignmentUniqueCost.startDate,
-                assignmentUniqueCost.endDate);
-        pulpAssignmentUniqueCost.tasks = userStoriesToTasks(assignmentUniqueCost.userStories, assignmentUniqueCost.relationHoursPoints);
-        return pulpAssignmentUniqueCost;
-    }
-
+    const pulpAssignmentUniqueCost: PulpAssignmentUniqueCost = new PulpAssignmentUniqueCost();
+    pulpAssignmentUniqueCost.agents =
+        developersToAgents(
+            assignmentUniqueCost.developers,
+            assignmentUniqueCost.startDate,
+            assignmentUniqueCost.endDate);
+    pulpAssignmentUniqueCost.tasks = userStoriesToTasks(assignmentUniqueCost.userStories, assignmentUniqueCost.relationHoursPoints);
+    return pulpAssignmentUniqueCost;
+}
+export function assignmentWithAttributesToPulpAssignmentWithAttributes
+    (assignmentByPunctuation: AssignmentByPunctuation): PulpAssignmentWithAttributes {
+    const pulpAssignmentWithAttributes: PulpAssignmentWithAttributes = new PulpAssignmentWithAttributes();
+    pulpAssignmentWithAttributes.agents = developersToAgentsWithAttributes(assignmentByPunctuation.developers);
+    pulpAssignmentWithAttributes.tasks = userStoriesToTaskWithAttributes(assignmentByPunctuation.userStories);
+    pulpAssignmentWithAttributes.assign_same_quantity_of_tasks = assignmentByPunctuation.assign_same_quantity_of_tasks;
+    return pulpAssignmentWithAttributes; 
+}
 export function pulpAssignmentUniqueCostResponseToassignementUniqueCost(
     pulpAssignmentUniqueCostResponse: PulpAssignmentUniqueCostResponse,
     assignmentUniqueCost: AssignmentUniqueCost
 ): AssignmentUniqueCost {
-    for (const id_agent in pulpAssignmentUniqueCostResponse) {
-        for (const idUserStory of pulpAssignmentUniqueCostResponse[id_agent]) {
-        console.log(assignmentUniqueCost.userStories, "historias de usuario desde pulpassignment conv");
-        console.log(idUserStory, "Id historia de usuario desde pulp conv");
+    for (const id_agent in pulpAssignmentUniqueCostResponse.assignments) {
+        for (const idUserStory of pulpAssignmentUniqueCostResponse.assignments[id_agent]) {
             assignmentUniqueCost.userStories.find(userStory => Number(userStory.id) === Number(idUserStory)).assigned_to = id_agent;
         }
     }
@@ -40,10 +53,43 @@ export function pulpAssignmentUniqueCostResponseToassignementUniqueCost(
 export function userStoryToTask(userStory: UserStory, relationHoursPoints: number): Task {
     const task = new Task();
     task.external_id = userStory.id;
-    task.cost = userStory.total_points  * relationHoursPoints;
+    task.cost = userStory.total_points * relationHoursPoints;
     return task;
 }
+export function pulpAssignmentWithAttributesResponseToassignementByPunctuations(
+    pulpAssignmentWithAttributesResponse: PulpAssignmentWithAttributesResponse,
+    assignmentByPunctuations: AssignmentByPunctuation
+): AssignmentByPunctuation {
+    for (const id_agent in pulpAssignmentWithAttributesResponse.assignments) {
+        for (const idUserStory of pulpAssignmentWithAttributesResponse.assignments[id_agent]) {
+            assignmentByPunctuations.userStories.find(userStory => Number(userStory.id) === Number(idUserStory)).assigned_to = id_agent;
+        }
+    }
+    return assignmentByPunctuations;
+}
+export function userStoryToTaskWithAttributes(userStory: UserStory): TaskWithAtributes {
+    const taskWithAttributes = new TaskWithAtributes();
+    taskWithAttributes.external_id = userStory.id;
+    taskWithAttributes.attributes_punctuation = punctuationsToPulpPunctuations(userStory.punctuations);
+    return taskWithAttributes;
+}
+export function userStoriesToTaskWithAttributes(userStories: UserStory[]): TaskWithAtributes[] {
+    return userStories.map(userStoryToTaskWithAttributes);
+}
+function punctuationToTaskPunctuation(punctuation: Punctuation): AtributePunctuation {
+    const attributePunctuation = new AtributePunctuation();
+    attributePunctuation.external_id = punctuation.id;
+    attributePunctuation.punctuation = punctuation.value? punctuation.value: 0;
+    return attributePunctuation;
+}
+function punctuationsToPulpPunctuations(punctuations: Punctuation[]): AtributePunctuation[] {
+    return punctuations.map(punctuationToTaskPunctuation);
+}
 
+
+export function userStoriesToTasks(userStories: UserStory[], relationHoursPoints: number): Task[] {
+    return userStories.map(function (element) { return userStoryToTask(element, relationHoursPoints) });
+}
 export function developerToAgent(
     developer: Developer,
     workStartDate: Date,
@@ -55,30 +101,34 @@ export function developerToAgent(
     console.log(agent, 'agent from user stories asignment');
     return agent;
 }
-
-export function userStoriesToTasks( userStories: UserStory [], relationHoursPoints: number): Task[] {
-    return userStories.map(function (element) {return userStoryToTask(element, relationHoursPoints)});
-}
-
 export function developersToAgents(
     developers: Developer[],
     workStartDate: Date,
     workEndDate: Date): Agent[] {
-        return developers.map(
-            function(developer) {
-                return developerToAgent(developer, workStartDate, workEndDate);
-            }
-        );
-    }
+    return developers.map(
+        function (developer) {
+            return developerToAgent(developer, workStartDate, workEndDate);
+        }
+    );
+}
 
+export function developerToAgentWithAttributes(developer: Developer): AgentWithAttributes {
+    let agentWithAttributes = new AgentWithAttributes();
+    agentWithAttributes.external_id = developer.id;
+    agentWithAttributes.attributes_punctuation = punctuationsToPulpPunctuations(developer.punctuations);
+    return agentWithAttributes;
+}
+export function developersToAgentsWithAttributes(developers: Developer[]): AgentWithAttributes[] {
+    return developers.map(developerToAgentWithAttributes);
+}
 function getBusinessDatesCount(startDate: Date, endDate: Date) {
     let count = 0;
     const curDate: Date = new Date(startDate);
-    const endDateD = new Date (endDate);
+    const endDateD = new Date(endDate);
     while (curDate <= endDateD) {
         const dayOfWeek = curDate.getDay();
         if (!((dayOfWeek == 6) || (dayOfWeek == 0)))
-           count++;
+            count++;
         curDate.setDate(curDate.getDate() + 1);
     }
     return count;
